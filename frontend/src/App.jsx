fe139e65ddb99ca0817dd50a7b95aa150de3e11a
ohjinseo <ojs835@naver.com>
2023-01-18 15:18:54 +0900

    feat : ElctronJS 연동

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
-import Navbar from "./components/Navbar";
-import Board from "./pages/Board";
 import Home from "./pages/Home";
-import Recipe from "./pages/Recipe";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/recipe" element={<Recipe />} />
         </Routes>
       </BrowserRouter>
     </>
