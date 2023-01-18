51938cfd517782dc2fae569c22dede44612e5883
ohjinseo <ojs835@naver.com>
2023-01-18 15:44:36 +0900

    feat : css reset

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import Home from "./pages/Home";
+import reset from 'styled-reset'
+import { createGlobalStyle } from 'styled-components'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
